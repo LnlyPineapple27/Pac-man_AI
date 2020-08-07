54e0832abc9377a47556040ff1fa2d3411c049b4
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-08-07 18:38:55 +0700

    ADD: message box when died or win

--- Pacman/UI.py
+++ Pacman/UI.py
@@ -1,38 +1,35 @@
-import turtle
-import random
 import math
 import sys
+import turtle
 from Maze import *
+from tkinter import messagebox
 
-if __name__ == "__main__":
-    input_list = InputHandle()
-    input_list.items()
-    maze = input_list.get_maze()
-    maze.print_raw_data()
-    maze.print_entities()
-    difficulty = 1
-    startGame(maze, difficulty)
+
+EMPTY = 0
+WALL = 1
+TREAT = 2
+MONSTER = 3
 
 # --------------------------------------------Initial things-----------------------------
 window = turtle.Screen()
 window.bgcolor('black')
-window.title('Maze Game')
+window.title('AI Pacman')
 window.setup(700, 700)
 window.tracer(0)
 # --------------------------------------------------
 images = ["..\\images\\gif\\Blue_left.gif",
-            "..\\images\\gif\\Red_left.gif",
-            "..\\images\\gif\\Pink_left.gif",
-            "..\\images\\gif\\Orange_left.gif",
-            "..\\images\\gif\\Blue_right.gif",
-            "..\\images\\gif\\Red_right.gif",
-            "..\\images\\gif\\Pink_right.gif",
-            "..\\images\\gif\\Orange_right.gif",
-            "..\\images\\gif\\pacman_right.gif",
-            "..\\images\\gif\\pacman_left.gif",
-            "..\\images\\gif\\pacman_up.gif",
-            "..\\images\\gif\\pacman_down.gif",
-            "..\\images\\gif\\fruit.gif"]
+          "..\\images\\gif\\Red_left.gif",
+          "..\\images\\gif\\Pink_left.gif",
+          "..\\images\\gif\\Orange_left.gif",
+          "..\\images\\gif\\Blue_right.gif",
+          "..\\images\\gif\\Red_right.gif",
+          "..\\images\\gif\\Pink_right.gif",
+          "..\\images\\gif\\Orange_right.gif",
+          "..\\images\\gif\\pacman_right.gif",
+          "..\\images\\gif\\pacman_left.gif",
+          "..\\images\\gif\\pacman_up.gif",
+          "..\\images\\gif\\pacman_down.gif",
+          "..\\images\\gif\\fruit.gif"]
 for img in images:
     turtle.register_shape(img)
 
@@ -44,6 +41,7 @@ class Wall(turtle.Turtle):
         self.color('blue')
         self.penup()
         self.speed(0)
+
 
 class Player(turtle.Turtle):
     def __init__(self):
@@ -58,29 +56,29 @@ class Player(turtle.Turtle):
         move_to_x = self.xcor()
         move_to_y = self.ycor() + 24
         self.shape("..\\images\\gif\\pacman_up.gif")
-        if (move_to_x,move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
 
     def go_down(self):
         move_to_x = self.xcor()
         move_to_y = self.ycor() - 24
         self.shape("..\\images\\gif\\pacman_down.gif")
-        if (move_to_x,move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
 
     def go_left(self):
         move_to_x = self.xcor() - 24
         move_to_y = self.ycor()
         self.shape("..\\images\\gif\\pacman_left.gif")
-        if (move_to_x,move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
 
     def go_right(self):
         move_to_x = self.xcor() + 24
         move_to_y = self.ycor()
         self.shape("..\\images\\gif\\pacman_right.gif")
-        if (move_to_x,move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
 
     def exit(self):
         self.goto(self.xcor(), self.ycor())
@@ -94,6 +92,11 @@ class Player(turtle.Turtle):
         else:
             return False
 
+    def destroy(self):
+        self.goto(2000, 2000)
+        self.hideturtle()
+
+
 class Enemy(turtle.Turtle):
     def __init__(self, x, y):
         turtle.Turtle.__init__(self)
@@ -103,8 +106,9 @@ class Enemy(turtle.Turtle):
         self.speed(0)
         # self.gold = 25
         self.goto(x, y)
-        self.direction = random.choice(["up","down","right","left"])
+        self.direction = random.choice(["up", "down", "right", "left"])
         # "up", "down",
+
     def move(self):
         if self.direction == "up":
             dx = 0
@@ -126,41 +130,41 @@ class Enemy(turtle.Turtle):
 
         if self.is_close(player):
             if player.xcor() < self.xcor():
-              self.direction = 'left'
+                self.direction = 'left'
             if player.xcor() > self.xcor():
                 self.direction = 'right'
             if player.ycor() < self.ycor():
                 self.direction = 'down'
             if player.ycor() > self.ycor():
                 self.direction = 'up'
-        #Calculate the spot to move to
+        # Calculate the spot to move to
         move_to_x = self.xcor() + dx
         move_to_y = self.ycor() + dy
 
-        #Check if space is a wall
-        if(move_to_x, move_to_y) not in walls:
+        # Check if space is a wall
+        if (move_to_x, move_to_y) not in walls:
             self.goto(move_to_x, move_to_y)
         else:
-            #Choose a different direction
+            # Choose a different direction
             self.direction = random.choice(["up", "down", "left", "right"])
 
-        #Set timer to move next time
+        # Set timer to move next time
         turtle.ontimer(self.move, t=random.randint(10, 30))
 
-
     def is_close(self, other):
-        a = self.xcor()-other.xcor()
-        b = self.ycor()-other.ycor()
-        distance = math.sqrt((a ** 2) + (b ** 2) )
+        a = self.xcor() - other.xcor()
+        b = self.ycor() - other.ycor()
+        distance = math.sqrt((a ** 2) + (b ** 2))
 
         if distance < 75:
-          return True
+            return True
         else:
-          return False
+            return False
 
     def destroy(self):
         self.goto(2000, 2000)
         self.hideturtle()
+
 
 class Treasure(turtle.Turtle):
     def __init__(self, x, y):
@@ -173,9 +177,8 @@ class Treasure(turtle.Turtle):
         self.goto(x, y)
 
     def destroy(self):
-       self.goto (2000,2000)
-       self.hideturtle()
-
+        self.goto(2000, 2000)
+        self.hideturtle()
 
 
 # Global variable
@@ -185,69 +188,84 @@ walls = ['']
 walls_block = Wall()
 player = Player()
 
+
 # start position of character
 def setup_maze(board, difficulty, init_index):
+
     init_col = init_index[0]
     init_row = init_index[1]
     for i in range(len(board)):
         for j in range(len(board[i])):
             # get the character of each x,y coord
 
-            character = board[i][j]
+            unity = board[i][j]
             screen_x = -288 + (j * 24)
             screen_y = 288 - (i * 24)
             # printing the maze
 
-            if character == 1:
+            if unity == WALL:
                 walls_block.goto(screen_x, screen_y)
                 # walls_block.shape('Wall.gif')
                 walls_block.stamp()
                 # Add co-ordinates to list
-                walls.append((screen_x,screen_y))
+                walls.append((screen_x, screen_y))
 
-            if character == 2:
+            if unity == TREAT:
                 treasures.append(Treasure(screen_x, screen_y))
 
-            if character == 3 and difficulty != 1:
+            if unity == MONSTER and difficulty != 1:
                 enemies.append(Enemy(screen_x, screen_y))
-    # print Player acording to its given location
+    # print Player according to its given location
     player.goto(-288 + (init_col * 24), 288 - (init_row * 24))
+
 
 def startGame(data: Maze, difficulty):
     setup_maze(data.maze_data, difficulty, data.pacman_init_position)
     turtle.listen()
-    turtle.onkey(player.go_up,'Up')
-    turtle.onkey(player.go_down,'Down')
-    turtle.onkey(player.go_right,'Right')
-    turtle.onkey(player.go_left,'Left')
+    turtle.onkey(player.go_up, 'Up')
+    turtle.onkey(player.go_down, 'Down')
+    turtle.onkey(player.go_right, 'Right')
+    turtle.onkey(player.go_left, 'Left')
 
     # Initiate motion of the enemies
     for enemy in enemies:
-        turtle.ontimer(enemy.move, t = 250)
+        turtle.ontimer(enemy.move, t=250)
 
-    while True:
+    treats_left = len(treasures)
+    died = False
+    while treats_left or not died :
         for treasure in treasures:
             if player.is_collision(treasure):
-                #Add the treasure gold to the player gold
+                # Desc treats left
+                treats_left -= 1
+                # Add the treasure gold to the player gold
                 player.gold += treasure.gold
-                print ('Player Gold: {}'.format(player.gold))
-                #Destroy the treasure
+                print('Player Gold: {}'.format(player.gold))
+                # Destroy the treasure
                 treasure.destroy()
-                #Remove the treasure
+                # Remove the treasure
                 treasures.remove(treasure)
 
         for enemy in enemies:
             if player.is_collision(enemy):
-                print ("Player died!!")
-                sys.exit()
-        #Update screen
+                print("Player died!!")
+                player.destroy()
+                died = True
+
+        # Update screen
         window.update()
+        # check goal
+        if not treats_left:
+            messagebox.showinfo("Boom Surprise Madafaka", "You WON")
+        if died:
+            messagebox.showinfo("Boom Surprise Madafaka", "You DIED")
+            sys.exit()
 
 if __name__ == "__main__":
     input_list = InputHandle()
     input_list.items()
-    maze = input_list.get_maze()
+    maze = input_list.get_maze("data1.txt")
     maze.print_raw_data()
     maze.print_entities()
-    difficulty = 1
+    difficulty = 3
     startGame(maze, difficulty)
